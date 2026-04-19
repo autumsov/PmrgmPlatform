1668581f1e789a0005d407f08f766c8c24310f7f
autumsov <owanoke2@gmail.com>
2026-04-19 20:46:57 +0700

    Update final untuk pengumpulan tugas PjBL

--- app-toko/app.js
+++ app-toko/app.js
@@ -1,5 +1,4 @@
-// ===== KONFIGURASI =====
-const API_URL = 'http://localhost/api-toko/get_barang.php';
+const API_URL = '../api-toko/get_barang.php';
 
 // ===== ELEMEN DOM =====
 const tbodyEl       = document.getElementById('tabel-barang');
